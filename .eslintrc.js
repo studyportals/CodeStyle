c40e1da3cfded319f64ff1b9a827062eeb156bb1
Rob van den Hout <Alfalfamale@users.noreply.github.com>
2017-06-16 14:05:44 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+//http://eslint.org/docs/rules/
+/*global module*/
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "globals": {},
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": "off",
+	      "no-mixed-spaces-and-tabs": ["error", "smart-tabs"]
+    }
+}
