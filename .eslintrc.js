df858c21489733cc3dece69d1c446508f165e7c7
stefanklokgieters <stefan@studyportals.com>
2019-07-19 14:42:00 +0200

    Indentation fix.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,10 @@ module.exports = {
     },
     "parserOptions": {
         "sourceType": "module",
-	"ecmaVersion": 2017,
-	"ecmaFeatures": {
-		"experimentalObjectRestSpread": true
-	}
+		"ecmaVersion": 2017,
+		"ecmaFeatures": {
+			"experimentalObjectRestSpread": true
+		}
     },
     "globals": {
     	"$": true,
